609ddd74f26945cde79537e7d215881e3e68ca77
Damien <damiensporting@gmail.com>
2020-12-06 14:58:32 +0100

    <docs> Adding documentation and comments

--- src/main.adb
+++ src/main.adb
@@ -2,19 +2,22 @@ With Ada.Text_IO;           use Ada.Text_IO;
 With Ada.Numerics.Discrete_Random;
 
 procedure main is 
+    -- number that can be put in a suboku Cell
     subtype Number is Natural range 1..9;
     package Discrete_Random is new Ada.Numerics.Discrete_Random(number);
     use Discrete_Random;
     Rand : Generator;
-    type T_Case is record
+    -- type of a Cell in the sudoku grid
+    type T_Cell is record
         Nb : Number;
         Visible : Boolean;
     end record;
-    type Line is array (Number) of T_Case;
+    -- theses array are index by Number so they are indexed from 1 to 9
+    type Line is array (Number) of T_Cell;
     type Grid is array (Number) of Line;
     type Possible_Nb is array(Number) of Boolean;
 
-
+    -- Display a Sudoku grid in the console
     procedure Put_Line(g : grid) 
     is
         H_Line : constant string(1..22):=(others=>'-');
@@ -38,6 +41,7 @@ procedure main is
         end loop;
     end Put_Line;
 
+    -- Display an Array of boolean, useful for debugging
     procedure Put_line(p : Possible_Nb) 
     is
     begin
@@ -48,18 +52,12 @@ procedure main is
         new_line;
     end Put_line;
 
-    procedure Put_line(p : Line) 
-    is
-    begin
-        for e of p loop
-            put(e.Nb'Image);
-            put(";");
-        end loop;
-        new_line;
-    end Put_line;
 
-
-    function Find_Possible_Nb(G : Grid; x :  Number; y : Number; X_Cube : Number; Y_Cube : Number; nb_possibility : out Natural) return Possible_Nb
+    -- This Function return an boolean array of the possible numbers of a sudoku Cell and the number of differents values possibility for this Cell
+    -- It use only visible Cell on the grid
+    -- It need the grid, the Cell coordonates, the top left Cell coordonates of the square
+    -- Number of possibility musb be a  Natural because it can be 0
+    function Find_Possible_Nb(G : Grid; x :  Number; y : Number; X_square : Number; Y_square : Number; nb_possibility : out Natural) return Possible_Nb
     is
         Possible_Numbers : Possible_Nb:=(others=>True);
     begin
@@ -73,8 +71,8 @@ procedure main is
         end loop;
         for i in 0..2 loop
             for k in 0..2 loop
-                if G(Y_Cube+i)(X_Cube+k).Visible then
-                    Possible_Numbers(G(Y_Cube+i)(X_Cube+k).Nb):=False;
+                if G(Y_square+i)(X_square+k).Visible then
+                    Possible_Numbers(G(Y_square+i)(X_square+k).Nb):=False;
                 end if;
             end loop;
         end loop;
@@ -88,10 +86,16 @@ procedure main is
     end Find_Possible_Nb;
 
 
+    -- This function generates a Valide Sudoku grid with all the Cells visible
+    -- This function returns True or False whether the generation is successful or not
+    -- It's a backtracting algorithm
+    -- The Input grid must be empty, default value (Nb=1,Visible=>False)
     function Generate_Grid(G : in out grid; x : Number:=1; y : Number:=1) return Boolean
     is
 
-        function Get_Case_In_Possibility(Possible_Numbers : Possible_Nb; Values_Already_tested : in out Possible_Nb) return T_Case
+        -- this function returns a sudoku Cell that wasn't test before
+        -- this function needs the Possible numbers of this Cell and the Values already tested
+        function Get_Cell_In_Possibility(Possible_Numbers : Possible_Nb; Values_Already_tested : in out Possible_Nb) return T_Cell
         is
             ind : Natural:=Random(Rand);
             i : Number :=1;
@@ -111,10 +115,10 @@ procedure main is
                 end if;
             end loop;
             return (Nb=>i,Visible=>True);
-        end Get_Case_In_Possibility;
+        end Get_Cell_In_Possibility;
 
-        X_Cube : Number:=((x-1)/3)*3+1;
-        Y_Cube : Number:=((y-1)/3)*3+1; 
+        X_square : Number:=((x-1)/3)*3+1;
+        Y_square : Number:=((y-1)/3)*3+1; 
         next_X : Number;
         next_y : Number;
         nb_possibility : Natural;
@@ -123,15 +127,15 @@ procedure main is
         Values_Already_tested : Possible_Nb:=(others => False);
 
     begin
-        Possible_Numbers:=Find_Possible_Nb(g,x,y,X_Cube,Y_Cube,nb_possibility);
+        Possible_Numbers:=Find_Possible_Nb(g,x,y,X_square,Y_square,nb_possibility);
         if nb_possibility=0 then
-            return false;
+            return false; -- no posibilities we go back
         end if;
-        G(y)(x):=Get_Case_In_Possibility(Possible_Numbers,Values_Already_tested);
+        G(y)(x):=Get_Cell_In_Possibility(Possible_Numbers,Values_Already_tested);
         Value_test:=Value_test+1;
 
         if x=9 and y=9 then
-            return true;
+            return true; -- Last Cell complete
         end if;
         if x=9 then
             next_X:=1;
@@ -142,19 +146,21 @@ procedure main is
         end if;
         while not Generate_Grid(g,next_X,next_y) loop
             if  Value_test>=nb_possibility then
-                G(y)(x):=(1,False);
-                return false;
+                G(y)(x):=(1,False); -- set the actual Cell with default value
+                return false; -- if all the possiblities are tested and we still can't go further we go back 
             else
-                G(y)(x):=Get_Case_In_Possibility(Possible_Numbers,Values_Already_tested);
+                G(y)(x):=Get_Cell_In_Possibility(Possible_Numbers,Values_Already_tested);
                 Value_test:=Value_test+1;
             end if;
         end loop;
-        return true;
+        return true; -- if everything goes well
     end Generate_Grid;
 
+    -- this function transform the Input Grid (the Solution) into a Playable grid with a unique Solution
     function Playable_Grid(g :grid) return grid 
     is
-        procedure Hide_Case(g : in out grid) is
+        -- this function hide a random Cell
+        procedure Hide_Cell(g : in out grid) is
             x : Number:=Random(Rand);
             y : Number:=Random(Rand);
         begin
@@ -163,8 +169,9 @@ procedure main is
                 y:=Random(Rand);
             end loop;
             g(y)(x).Visible:=False;
-        end Hide_Case;
+        end Hide_Cell;
 
+        -- this function returns true if all the values of the grid are Visible
         function Is_Complete(G : Grid) return boolean 
         is
         begin
@@ -178,30 +185,30 @@ procedure main is
             return True;
         end Is_Complete;
                     
-        Playable_G : grid:=g;
-        tmp_G : grid:=Playable_G;
-        Test_Temp_G : grid:=Playable_G;
+        Playable_G : grid:=g; --grid that we will return
+        tmp_G : grid:=Playable_G; -- same grid as Playable_G grid but with another Cell Hide
+        Test_Temp_G : grid:=Playable_G; -- same grid as tmp_G that we will try to solve
         Changed : boolean;
-        X_Cube : Number;
-        Y_Cube : Number;
+        X_square : Number;
+        Y_square : Number;
         Possible_Numbers : Possible_Nb;
         Nb_Of_Possibility : Natural;
-        Nb_Hiding_Cases : Natural:=0;
+        Nb_Hiding_Cells : Natural:=0;
     begin
-        while Is_Complete(Test_Temp_G) loop
+        while Is_Complete(Test_Temp_G) loop -- if we still manage to complete the grid we try to hide another Cell
            Playable_G:=tmp_G;  
-           Hide_Case(tmp_G);
-           Nb_Hiding_Cases:=Nb_Hiding_Cases+1;
+           Hide_Cell(tmp_G);
+           Nb_Hiding_Cells:=Nb_Hiding_Cells+1;
            Test_Temp_G:=tmp_G;
            Changed:=True;
-           while Changed loop
+           while Changed loop -- if we have found a Cell that can be played we search for another Cell that can be played
                Changed:=False;
                for y in 1..9 loop
                    for x in 1..9 loop
                        if not Test_Temp_G(y)(x).Visible then
-                           X_Cube :=((x-1)/3)*3+1;
-                           Y_Cube :=((y-1)/3)*3+1; 
-                           Possible_Numbers:=Find_Possible_Nb(Test_Temp_G,x,y,X_Cube,Y_Cube,Nb_Of_Possibility);
+                           X_square :=((x-1)/3)*3+1;
+                           Y_square :=((y-1)/3)*3+1; 
+                           Possible_Numbers:=Find_Possible_Nb(Test_Temp_G,x,y,X_square,Y_square,Nb_Of_Possibility);
                            if Nb_Of_Possibility=1 then
                                Test_Temp_G(y)(x).Visible:=True;
                                Changed:=True;
@@ -211,8 +218,8 @@ procedure main is
                 end loop;
             end loop;
         end loop;
-        put_line("Number of Hiding Cases :"&Nb_Hiding_Cases'Image);
-        put_line("Number of Visible Cases :"&Integer'Image(91-Nb_Hiding_Cases));
+        put_line("Number of Hiding Cells :"&Nb_Hiding_Cells'Image);
+        put_line("Number of Visible Cells :"&Integer'Image(91-Nb_Hiding_Cells));
         new_line;
         return Playable_G;
         end Playable_Grid;
